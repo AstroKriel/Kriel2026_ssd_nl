7f509f044f01d2243070c768273ed6cade5661bd
neco kriel <necokriel@gmail.com>
2025-07-29 17:45:37 +1000

    update routines to include physical constraints for t_nl and t_sat

--- scripts/fit_time_evolution/my_mcmc_fitting_routine/mcmc_stage_2_linear.py
+++ scripts/fit_time_evolution/my_mcmc_fitting_routine/mcmc_stage_2_linear.py
@@ -3,6 +3,8 @@
 ## ###############################################################
 
 import numpy
+from scipy.ndimage import gaussian_filter1d
+from jormi.utils import list_utils
 from . import base_mcmc
 from . import mcmc_utils
 
@@ -21,93 +23,116 @@ class Stage2MCMCRoutine_linear(base_mcmc.BaseMCMCRoutine):
       std_energy_values  : list | numpy.ndarray,
       initial_params     : tuple[float, ...],
       prior_kde          : callable = None,
-      plot_posterior_kde : bool = True
+      plot_posterior_kde : bool = True,
     ):
-    self.max_time = numpy.max(time_values)
+    guess_sat_time = self._define_constraints(time_values, ave_energy_values)
     super().__init__(
       routine_name        = "stage2_linear",
       output_directory    = output_directory,
       x_values            = time_values,
       y_values            = ave_energy_values,
       likelihood_sigma    = std_energy_values,
-      initial_params      = initial_params,
+      initial_params      = (
+        initial_params[0], # log10(E_init)
+        initial_params[1], # log10(E_sat)
+        initial_params[2], # exp_gamma
+        initial_params[3], # t_nl
+        guess_sat_time # t_sat
+      ),
       prior_kde           = prior_kde,
       plot_posterior_kde  = plot_posterior_kde,
       data_label          = r"$E_{\mathrm{mag}}$",
       fitted_param_labels = [
         r"$\log_{10}(E_{\mathrm{init}})$",
         r"$\log_{10}(E_{\mathrm{sat}})$",
-        r"$\gamma$",
+        r"$\gamma_\mathrm{exp}$",
         r"$t_{\mathrm{nl}}$",
-        r"$t_{\mathrm{sat}}$",
+        r"$t_{\mathrm{sat}}$"
       ]
     )
+
+  def _define_constraints(self, time_values, ave_energy_values):
+    self.max_sim_time = numpy.max(time_values)
+    ## define max time to transition into saturated phase
+    dlny_dt = gaussian_filter1d(numpy.gradient(numpy.log10(ave_energy_values), time_values), sigma=2)
+    max_sat_time_index = list_utils.find_first_crossing(values=dlny_dt, target=0)
+    self.max_sat_time  = time_values[max_sat_time_index]
+    ## define max time to transition into nonlinear phase
+    ## note, make sure this happens before the saturated phase
+    dy_dt = numpy.gradient(ave_energy_values, time_values)
+    target_dy_dt = 0.5 * numpy.max(dy_dt[:max_sat_time_index])
+    max_nl_time_index = list_utils.find_first_crossing(values=dy_dt[:max_sat_time_index], target=target_dy_dt)
+    self.max_nl_time = time_values[max_nl_time_index]
+    ## construct a valid guess for the transition time into the saturated phase
+    guess_sat_time = self.max_nl_time + 0.5 * (self.max_sat_time - self.max_nl_time)
+    return guess_sat_time
 
   def _model(self, param_vectors):
     param_vectors = numpy.atleast_2d(param_vectors) # (N, P)
     ## output dimensions
     num_local_walkers = param_vectors.shape[0] # N
     num_data_points = len(self.x_values) # T
-    ## unpack model parameters (P = 5)
-    log10_init_energy, log10_sat_energy, gamma, start_nl_time, start_sat_time = param_vectors.T
+    ## unpack model parameters (P = 6)
+    log10_init_energy, log10_sat_energy, exp_gamma, nl_start_time, sat_start_time = param_vectors.T
     ## reshape parameters to allow for vectorising over param-rows
     x_values_2d        = self.x_values[None, :] # shape (1, T)
-    start_nl_time_2d   = start_nl_time[:, None] # shape (N, 1)
-    start_sat_time_2d  = start_sat_time[:, None] # shape (N, 1)
-    gamma_2d           = gamma[:, None] # shape (N, 1)
+    exp_gamma_2d       = exp_gamma[:, None] # shape (N, 1)
+    nl_start_time_2d   = nl_start_time[:, None] # shape (N, 1)
+    sat_start_time_2d  = sat_start_time[:, None] # shape (N, 1)
     ## mask SSD phases
-    mask_exp_phase     = x_values_2d < start_nl_time_2d
-    mask_nl_phase      = (start_nl_time_2d <= x_values_2d) & (x_values_2d < start_sat_time_2d)
-    mask_sat_phase     = start_sat_time_2d < x_values_2d
+    mask_exp_phase     = x_values_2d < nl_start_time_2d
+    mask_nl_phase      = (nl_start_time_2d <= x_values_2d) & (x_values_2d < sat_start_time_2d)
+    mask_sat_phase     = sat_start_time_2d < x_values_2d
     ## compute model constants (per walker)
     init_energy        = 10**log10_init_energy # (N,)
     init_energy_2d     = init_energy[:, None] # (N, 1)
     sat_energy         = 10**log10_sat_energy # (N,)
     sat_energy_2d      = sat_energy[:, None] # (N, 1)
-    start_nl_energy    = init_energy * numpy.exp(gamma * start_nl_time) # (N,)
-    start_nl_energy_2d = start_nl_energy[:, None] # (N, 1)
-    alpha              = (sat_energy - start_nl_energy) / (start_sat_time - start_nl_time) # (N,)
-    alpha_2d           = alpha[:, None] # (N, 1)
+    nl_start_energy    = init_energy * numpy.exp(exp_gamma * nl_start_time) # (N,)
+    nl_start_energy_2d = nl_start_energy[:, None] # (N, 1)
+    nl_gamma           = (sat_energy - nl_start_energy) / (sat_start_time - nl_start_time) # (N,)
+    nl_gamma_2d        = nl_gamma[:, None] # (N, 1)
     ## assemble modelled SSD phases
     energy_2d = numpy.zeros((num_local_walkers, num_data_points))
-    energy_2d[mask_exp_phase] = (init_energy_2d * numpy.exp(gamma_2d * x_values_2d))[mask_exp_phase] # (N, T)
-    energy_2d[mask_nl_phase]  = (start_nl_energy_2d + alpha_2d * (x_values_2d - start_nl_time_2d))[mask_nl_phase] # (N, T)
+    energy_2d[mask_exp_phase] = (init_energy_2d * numpy.exp(exp_gamma_2d * x_values_2d))[mask_exp_phase] # (N, T)
+    energy_2d[mask_nl_phase]  = (nl_start_energy_2d + nl_gamma_2d * (x_values_2d - nl_start_time_2d))[mask_nl_phase] # (N, T)
     energy_2d[mask_sat_phase] = numpy.broadcast_to(sat_energy_2d, (num_local_walkers, num_data_points))[mask_sat_phase] # (N, T)
     return energy_2d
 
   def _get_valid_params_mask(self, param_vectors):
     param_vectors = numpy.atleast_2d(param_vectors)
     num_local_walkers = param_vectors.shape[0]
-    log10_init_energy, log10_sat_energy, gamma, start_nl_time, start_sat_time = param_vectors.T
+    log10_init_energy, log10_sat_energy, exp_gamma, nl_start_time, sat_start_time = param_vectors.T
     valid_log10_init_energy = (-30 < log10_init_energy) & (log10_init_energy < -5)
     valid_log10_sat_energy  = (-5 < log10_sat_energy) & (log10_sat_energy < 0)
-    valid_gamma             = (0 < gamma) & (gamma < 10)
-    valid_start_nl_time     = (0.1 * self.max_time < start_nl_time) & (start_nl_time < start_sat_time)
-    valid_start_sat_time    = start_sat_time < 0.9 * self.max_time
+    valid_exp_gamma         = (0 < exp_gamma) & (exp_gamma < 10)
+    valid_nl_start_time     = (nl_start_time < self.max_nl_time) & (nl_start_time < sat_start_time)
+    valid_sat_start_time    = sat_start_time < self.max_sat_time
     valid_params_mask = (
-      valid_log10_init_energy & valid_log10_sat_energy & valid_gamma & valid_start_nl_time & valid_start_sat_time
+      valid_log10_init_energy &
+      valid_log10_sat_energy &
+      valid_exp_gamma &
+      valid_nl_start_time &
+      valid_sat_start_time
     )
     if num_local_walkers == 1:
       return valid_params_mask[0]
     return valid_params_mask
 
   def _get_kde_params(self, param_vectors):
-    ## ignore the transition times: use a unifrom prior for them
+    ## ignore the transition times implicity gives them a unifrom prior
     return numpy.asarray(param_vectors[:, :3])
 
   def _annotate_fitted_params(self, axs):
-    init_energy_samples     = 10**self.fitted_posterior_samples[:,0]
     sat_energy_samples      = 10**self.fitted_posterior_samples[:,1]
-    gamma_samples           = self.fitted_posterior_samples[:,2]
-    start_nl_time_samples   = self.fitted_posterior_samples[:,3]
-    start_sat_time_samples  = self.fitted_posterior_samples[:,4]
-    start_nl_energy_samples = init_energy_samples * numpy.exp(gamma_samples * start_nl_time_samples)
-    alpha_samples           = (sat_energy_samples - start_nl_energy_samples) / (start_sat_time_samples - start_nl_time_samples)
+    nl_start_time_samples   = self.fitted_posterior_samples[:,3]
+    sat_start_time_samples  = self.fitted_posterior_samples[:,4]
     mcmc_utils.plot_param_percentiles(axs[0], sat_energy_samples, orientation="horizontal")
-    mcmc_utils.plot_param_percentiles(axs[1], alpha_samples, orientation="horizontal")
     for row_index in range(len(axs)):
-      mcmc_utils.plot_param_percentiles(axs[row_index], start_nl_time_samples, orientation="vertical")
-      mcmc_utils.plot_param_percentiles(axs[row_index], start_sat_time_samples, orientation="vertical")
+      mcmc_utils.plot_param_percentiles(axs[row_index], nl_start_time_samples, orientation="vertical")
+      mcmc_utils.plot_param_percentiles(axs[row_index], sat_start_time_samples, orientation="vertical")
+      axs[row_index].axvline(self.max_nl_time, color="red")
+      axs[row_index].axvline(self.max_sat_time, color="red")
 
 
 ## END OF MODULE

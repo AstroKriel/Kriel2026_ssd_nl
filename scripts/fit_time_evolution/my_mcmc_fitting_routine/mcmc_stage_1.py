be8616ad0fdec9490a939f41efcb4bbcc67c9c46
Neco Kriel <necokriel@gmail.com>
2025-08-18 14:05:55 +1000

    add correctness check: input guess params

--- scripts/fit_time_evolution/my_mcmc_fitting_routine/mcmc_stage_1.py
+++ scripts/fit_time_evolution/my_mcmc_fitting_routine/mcmc_stage_1.py
@@ -22,6 +22,9 @@ class Stage1MCMCRoutine(base_mcmc.BaseMCMCRoutine):
       initial_params          : tuple[float, ...],
       plot_posterior_kde      : bool = True
     ):
+    assert len(initial_params) == 3, (
+      "Stage 1 MCMC routine expects 3 initial params: log10(E_init), gamma_exp, and t_approx"
+    )
     self.log10_e = numpy.log10(numpy.exp(1))
     self.max_sim_time = numpy.max(time_values)
     super().__init__(
